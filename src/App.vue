a716e640cc3eb389c15ae8e967e5c109c84f8cab
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-01-25 23:58:23 +0700

    accointing done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 const positive = 'color: Green'
 const negative = 'color: Red'
-const accounting = [{
+const accountLists = [{
     name: 'salary',
     amount: 10000
 },
@@ -22,10 +22,14 @@ const accounting = [{
     amount: 5500
 }
 ]
+const netTotal = accountLists.reduce(function(previousValue, currentValue) {
+    return previousValue + currentValue.amount
+}, 0);
+
 </script>
 
 <template>
-<table class="border m-7 w-5/12">
+<table class="border m-7 w-3/6 sm:w-1/12 md:w-4/12 lg:w-8/12">
     <thead class="bg-slate-400">
         <th>
             Name
@@ -35,28 +39,23 @@ const accounting = [{
         </th>
     </thead>
     <tbody>
-        <tr v-for="value in accounting">
-            <td class="text-center" v-if="value.amount > 0">
-                {{ value.name }} 
+        <tr v-for="(cost, index) in accountLists" :key="index">
+            <td class="text-center" v-if="cost.amount > 0">
+                {{ cost.name }} 
             </td>
-            <td class="text-center" v-else-if="value.amount < 0">
-                {{ value.name }}
+            <td class="text-center" v-else-if="cost.amount < 0">
+                {{ cost.name }}
             </td>
-            <td class="text-center" v-if="value.amount > 0" :style="positive">
-                {{ value.amount }} 
+            <td class="text-center" v-if="cost.amount > 0" :style="positive">
+                {{ cost.amount }} 
             </td>
-            <td class="text-center" v-else-if="value.amount < 0" :style="negative">
-                {{ value.amount }}
+            <td class="text-center" v-else-if="cost.amount < 0" :style="negative">
+                {{ cost.amount }}
             </td>
         </tr>
     </tbody>
     <tfoot class="text-right ">
-        <th>
-            Net Total
-        </th>
-        <th v-for="cost in accounting">
-            
-        </th>
+        <p class="font-bold">Net Total - {{ netTotal}}</p>
     </tfoot>
 </table>
 
