52474c816f923561006c5b60f5afddb2f3340609
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-02-01 15:24:36 +0700

    update fot shorter

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,54 @@
 <script setup>
-const positive = 'color: Green'
-const negative = 'color: Red'
-const accountLists = [{
+const accounts = [
+  {
     name: 'salary',
-    amount: 10000
-},
-{
+    amount: 10000,
+  },
+  {
     name: 'rent',
-    amount: -2500
-},
-{
+    amount: -2500,
+  },
+  {
     name: 'parking fee',
-    amount: 0
-},
-{
+    amount: 0,
+  },
+  {
     name: 'car fuel',
-    amount: -500
-},
-{
+    amount: -500,
+  },
+  {
     name: 'commission',
-    amount: 5500
-}
+    amount: 5500,
+  },
 ]
-const netTotal = accountLists.reduce(function(previousValue, currentValue) {
-    return previousValue + currentValue.amount
-}, 0);
-
 </script>
 
 <template>
-<table class="border m-7 w-3/6 sm:w-1/12 md:w-4/12 lg:w-8/12">
-    <thead class="bg-slate-400">
-        <th>
-            Name
-        </th>
-        <th>
-            Amount
-        </th>
-    </thead>
-    <tbody>
-        <tr v-for="(cost, index) in accountLists" :key="index">
-            <td class="text-center" v-if="cost.amount > 0">
-                {{ cost.name }} 
-            </td>
-            <td class="text-center" v-else-if="cost.amount < 0">
-                {{ cost.name }}
-            </td>
-            <td class="text-center" v-if="cost.amount > 0" :style="positive">
-                {{ cost.amount }} 
-            </td>
-            <td class="text-center" v-else-if="cost.amount < 0" :style="negative">
-                {{ cost.amount }}
-            </td>
-        </tr>
-    </tbody>
-    <tfoot class="text-right ">
-        <p class="font-bold">Net Total - {{ netTotal}}</p>
-    </tfoot>
-</table>
-
-<!-- <div v-for="value in accounting">
-    <p v-if="value.amount > 0" :style="positive">{{ value.name }} {{value.amount}}</p>
-    <p v-else-if="value.amount < 0" :style="negative">{{ value.name }} {{value.amount}}</p>
-    <p v-else></p>
-</div> -->
+  <ul>
+    <li v-for="account in accounts">
+      <p v-if="account.amount !== 0">
+        <span> {{ account.name }} </span>:
+        <span
+          :style="
+            account.amount > 0
+              ? 'background-color: green'
+              : 'background-color: red'
+          "
+        >
+          {{ account.amount }}
+        </span>
+      </p>
+    </li>
+  </ul>
+  <p class="font-bold">
+    Net Total -
+    {{
+      accounts.reduce(
+        (previousValue, currentValue) => previousValue + currentValue.amount,
+        0
+      )
+    }}
+  </p>
 </template>
 
-<style>
-
-</style>
+<style></style>
